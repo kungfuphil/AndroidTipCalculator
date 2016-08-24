ff133dca1c00431f0526fc15ccfa61f483ff9d85
Philip Ho <phil@philho.com>
2016-08-23 21:27:52 -0400

    Added functionality to everything

--- AndroidTipCalculator/MainActivity.cs
+++ AndroidTipCalculator/MainActivity.cs
@@ -11,8 +11,6 @@ namespace AndroidTipCalculator
     [Activity(Label = "AndroidTipCalculator", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
-        int count = 1;
-
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
@@ -20,11 +18,67 @@ namespace AndroidTipCalculator
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
-            // Get our button from the layout resource,
-            // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.MyButton);
+            // Find controls for manipulation
+            var totalBillBeforeTip = FindViewById<EditText>(Resource.Id.txtTotalBeforeTip);
 
-            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+            //var taxRate = FindViewById<EditText>(Resource.Id.txtTaxRate);
+
+            RadioGroup radioGroup = FindViewById<RadioGroup>(Resource.Id.radioGroup1);
+            RadioButton tipPercent = FindViewById<RadioButton>(radioGroup.CheckedRadioButtonId);
+            RadioButton tenPercent = FindViewById<RadioButton>(Resource.Id.rdoTenPercent);
+            RadioButton fifteenPercent = FindViewById<RadioButton>(Resource.Id.rdoFifteenPercent);
+            RadioButton eighteenPercent = FindViewById<RadioButton>(Resource.Id.rdoEighteenPercent);
+            RadioButton twentyPercent = FindViewById<RadioButton>(Resource.Id.rdoTwentyPercent);
+
+            //var tip = FindViewById<TextView>(Resource.Id.lblTip);
+
+            //var grandTotal = FindViewById<TextView>(Resource.Id.lblGrandTotal);
+
+
+            // EVENTS
+            totalBillBeforeTip.TextChanged += (object sender, Android.Text.TextChangedEventArgs e) =>
+            {
+                CalculateAll();
+            };
+
+            tenPercent.Click += RadioTipPercentClick;
+            fifteenPercent.Click += RadioTipPercentClick;
+            eighteenPercent.Click += RadioTipPercentClick;
+            twentyPercent.Click += RadioTipPercentClick;
+        }
+
+        private void RadioTipPercentClick(object sender, EventArgs e)
+        {
+            CalculateAll();
+        }
+
+        public void CalculateAll()
+        {
+            var totalBillBeforeTip = FindViewById<EditText>(Resource.Id.txtTotalBeforeTip);
+
+            var taxRate = FindViewById<EditText>(Resource.Id.txtTaxRate);
+
+            RadioGroup radioGroup = FindViewById<RadioGroup>(Resource.Id.radioGroup1);
+            RadioButton tipPercent = FindViewById<RadioButton>(radioGroup.CheckedRadioButtonId);
+
+            var tip = FindViewById<TextView>(Resource.Id.lblTip);
+
+            var grandTotal = FindViewById<TextView>(Resource.Id.lblGrandTotal);
+
+            // If there is an amount in the total bill, then calculate the tip.
+            // Otherwise, remove the amounts in tip and grand total
+            if (totalBillBeforeTip.Text.Length > 0)
+            {
+                tip.Text = string.Format("{0:#.00}", (float.Parse(totalBillBeforeTip.Text) * (float.Parse(tipPercent.Text) / 100.0)));
+
+                grandTotal.Text = string.Format("{0:#.00}", (float.Parse(totalBillBeforeTip.Text) + float.Parse(tip.Text)));
+            }
+            else
+            {
+                tip.Text = "";
+                grandTotal.Text = "";
+            }
+
         }
     }
 }
--- AndroidTipCalculator/Resources/Resource.Designer.cs
+++ AndroidTipCalculator/Resources/Resource.Designer.cs
@@ -60,8 +60,47 @@ namespace AndroidTipCalculator
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f05000d
+			public const int lblGrandTotal = 2131034125;
+			
+			// aapt resource value: 0x7f05000b
+			public const int lblTip = 2131034123;
+			
+			// aapt resource value: 0x7f050005
+			public const int radioGroup1 = 2131034117;
+			
+			// aapt resource value: 0x7f050008
+			public const int rdoEighteenPercent = 2131034120;
+			
+			// aapt resource value: 0x7f050007
+			public const int rdoFifteenPercent = 2131034119;
+			
+			// aapt resource value: 0x7f050006
+			public const int rdoTenPercent = 2131034118;
+			
+			// aapt resource value: 0x7f050009
+			public const int rdoTwentyPercent = 2131034121;
+			
 			// aapt resource value: 0x7f050000
-			public const int MyButton = 2131034112;
+			public const int textView1 = 2131034112;
+			
+			// aapt resource value: 0x7f050002
+			public const int textView2 = 2131034114;
+			
+			// aapt resource value: 0x7f050004
+			public const int textView3 = 2131034116;
+			
+			// aapt resource value: 0x7f05000a
+			public const int textView4 = 2131034122;
+			
+			// aapt resource value: 0x7f05000c
+			public const int textView6 = 2131034124;
+			
+			// aapt resource value: 0x7f050003
+			public const int txtTaxRate = 2131034115;
+			
+			// aapt resource value: 0x7f050001
+			public const int txtTotalBeforeTip = 2131034113;
 			
 			static Id()
 			{
